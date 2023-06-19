170d79e27f9acb77af6948facde997a960a25656
Pranav Asrani <pranavnasrani@outlook.sg>
2023-06-19 19:01:42 +0800

    Initial commit

---
+++ data_collection_self.py
@@ -0,0 +1,150 @@
+import hashlib
+import os
+import random
+import tkinter
+from tkinter import filedialog
+
+import cv2
+import numpy as np
+import pyautogui
+import pygame
+from cv2.data import *
+
+pyautogui.alert('This is a highly elevated event. Do not edit the training data without reason. '
+                'This can lead'
+                'to unwanted changes in your system. For your security, password will be required to continue',
+                'Warning!')
+# password = '1r1s@PRAnav@)!)'
+password_os_256 = os.environ.get('PASSWORD_FOR_IRISMOUSE')
+
+input_pwd = pyautogui.password(text='(ssh) enter password: ', title='Password', default='', mask='*')
+
+if input_pwd is None:
+    exit()
+
+if password_os_256 == hashlib.sha256(input_pwd.encode()).hexdigest():
+    pyautogui.alert('CNN training activated.')
+
+    root = tkinter.Tk()
+    root.withdraw()  # use to hide tkinter window
+    current_dir = os.getcwd()
+    rootdir = filedialog.askdirectory(parent=root, initialdir=current_dir, title='Please select a directory to store images')
+    # Checking if root dir is selected. If not None is returned.
+
+    if rootdir is None:
+        exit()
+    rootdir = rootdir + '/'
+    if os.path.isdir(rootdir):
+        print('ok')
+        print('dir already exists. mkdir invalid, continuing...')
+        model_path = rootdir + 'iris_mouse.h5'
+        if os.path.exists(rootdir + 'iris_mouse.h5'):
+            os.remove(model_path)
+            pyautogui.alert('An existing model file was found. It was deleted.', 'Existing Model Found')
+
+    else:
+        print("dir does not exist. mkdir valid, continuing...")
+        os.mkdir(rootdir)
+else:
+    print('incorrect')
+    exit()
+
+# Initialize Pygame
+pygame.init()
+
+# Set screen size
+
+pygame.init()
+
+info = pygame.display.Info()
+w = info.current_w
+h = info.current_h
+
+
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, pygame.RESIZABLE)
+
+# Set dot color and position
+dot_color = (255, 0, 0)
+
+dot_pos = [249, 401]
+
+# Set dot speed
+dot_speed = 5
+
+# Set direction
+direction = [1, 1]
+
+
+# Normalization helper function
+def normalize(x):
+    minn, maxx = x.min(), x.max()
+    return (x - minn) / (maxx - minn)
+
+
+# Eye cropping function
+def scan(image_size=(32, 32)):
+    _, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    boxes = cascade.detectMultiScale(gray, 1.3, 10)
+    if len(boxes) == 2:
+        eyesl = []
+        for box in boxes:
+            x, y, w, h = box
+            eye = frame[y:y + h, x:x + w]
+            eye = cv2.resize(eye, image_size)
+            eye = normalize(eye)
+            eye = eye[10:-10, 5:-5]
+            eyesl.append(eye)
+        return (np.hstack(eyesl) * 255).astype(np.uint8)
+    else:
+        return None
+
+
+# Load Haar cascade classifier
+cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+# Open video capture
+video_capture = cv2.VideoCapture(0)
+
+# Set running flag
+running = True
+
+# Start loop
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                running = False
+
+    dot_pos[0] = random.randint(0, screen.get_width())
+    dot_pos[1] = random.randint(0, screen.get_height())
+
+    # Clear screen
+    screen.fill((0, 0, 0))
+
+    # Draw dot
+    pygame.draw.circle(screen, dot_color, dot_pos, 10)
+
+    # Update display
+    pygame.display.flip()
+
+    # Wait for a half second so eyes can adjust.
+    pygame.time.wait(800)
+    # Capture eyes image
+    eyes = scan()
+
+    # If eyes were detected
+    if not eyes is None:
+        x_dot = dot_pos[0]
+        y_dot = dot_pos[1]
+        print('eyes')  # Print 'eye' if an eye is detected.
+        # Save image to disk. Add 'eyes' so it can be split later.
+        filename = rootdir + "{} {} eyes.jpeg".format(x_dot, y_dot, rootdir)
+        cv2.imwrite(filename, eyes)
+
+# Quit Pygame and release video capture
+pygame.quit()
+video_capture.release()
---
+++ prediction_average_filter.py
@@ -0,0 +1,154 @@
+import sys
+import tkinter
+from tkinter import filedialog
+
+import cv2
+import keyboard
+import matplotlib.pyplot as plt
+import numpy as np
+import pyautogui
+from cv2.data import *
+from keras.layers import *
+from keras.models import *
+
+root = tkinter.Tk()
+root.withdraw()  # use to hide tkinter window
+current_dir = os.getcwd()
+rootdir = filedialog.askdirectory(parent=root, initialdir=current_dir,
+                                  title='Please select a directory to train CNN')
+# Checking if root dir is selected. If not None is returned.
+
+if rootdir is None:
+    sys.exit()
+
+cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+video_capture = cv2.VideoCapture(0)
+
+
+def normalize(x):
+    minn, maxx = x.min(), x.max()
+    return (x - minn) / (maxx - minn)
+
+
+def scan(image_size=(32, 32)):
+    _, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    boxes = cascade.detectMultiScale(gray, 1.3, 10)
+    if len(boxes) == 2:
+        eyes = []
+        for box in boxes:
+            x, y, w, h = box
+            eye = frame[y:y + h, x:x + w]
+            eye = cv2.resize(eye, image_size)
+            eye = normalize(eye)
+            eye = eye[10:-10, 5:-5]
+            eyes.append(eye)
+        return (np.hstack(eyes) * 255).astype(np.uint8)
+    else:
+        return None
+
+
+width, height = 1919, 1079
+
+check_model = os.path.isfile(rootdir + '/iris_mouse.h5')
+
+if not check_model:
+    opersys = pyautogui.prompt("What operating system are you using? \nWindows/Linux/Mac: ", 'OS')
+    if opersys is None:
+        sys.exit()
+    if opersys == "Windows":
+        root = rootdir + "\\"
+    elif opersys == "Mac":
+        root = rootdir + "/"
+    else:
+        root = rootdir + "/"
+
+    filepaths = os.listdir(root)
+    X, Y = [], []
+    # Split the filepath to x, y, and unwanted jpeg end.
+    # Then use x and y
+    for filepath in filepaths:
+        x, y, _ = filepath.split(' ')
+        x = float(x) / width
+        y = float(y) / height
+        X.append(cv2.imread(root + filepath))
+        Y.append([x, y])
+    X = np.array(X) / 255.0
+    Y = np.array(Y)
+    # print(X.shape, Y.shape)
+
+    pyautogui.alert('To exit the mouse system, press shift and x', 'Press Shift and x to exit.')
+    # Build the model
+    model = Sequential()
+    model.add(Conv2D(32, 3, 2, activation='relu', input_shape=(12, 44, 3)))
+    model.add(Conv2D(64, 2, 2, activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(2, activation='sigmoid'))
+    model.compile(optimizer="adam", loss="mean_squared_error")
+    # model.summary()
+
+    # Train the model and store the history object
+    history = model.fit(X, Y, epochs=210, batch_size=32, verbose=0)
+    # Get the training loss from the history object
+    train_loss = history.history['loss']
+
+    '''
+    epochs = 210
+    
+    for epoch in range(epochs):
+        model.fit(X, Y, batch_size=32)
+    '''
+    # Plot the training loss
+    plt.plot(train_loss, label='Training Loss')
+
+    # Add title and labels
+    plt.title('Training Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+
+    # Show the plot
+    plt.show()
+    # larger N value == more smoothing == more lag.
+    # Model did not exist so we reached here.
+    # Since it did not exist, we will be saving it in the cwd.
+    model.save(rootdir + '/iris_mouse.h5')
+
+else:
+    # Since model is found, we will load it...
+    pyautogui.alert('Model exists. Not training model.', 'Model found!. To exit press Shift and x')
+    model = load_model(rootdir + '/iris_mouse.h5')
+N = 3  # size of the filter scale
+weights = [0.1, 0.3, 0.6]  # weights for the weighted average
+x_buffer = [0] * N
+y_buffer = [0] * N
+
+while True:
+
+    if keyboard.is_pressed('shift') and keyboard.is_pressed('x'):
+        break
+    eyes = scan()
+    if not eyes is None:
+        eyes = np.expand_dims(eyes / 255.0, axis=0)
+        x, y = model.predict(eyes, verbose=0)[0]
+
+        # add new values to buffer
+        x_buffer.append(x)
+        y_buffer.append(y)
+
+        # remove oldest values from buffer
+        x_buffer.pop(0)
+        y_buffer.pop(0)
+
+        # compute weighted average of coordinates
+        x_smooth = sum(w * x for w, x in zip(weights, x_buffer))
+        y_smooth = sum(w * y for w, y in zip(weights, y_buffer))
+
+        pyautogui.moveTo(x_smooth * width, y_smooth * height)
+
+        pyautogui.FAILSAFE = False
+
+    else:
+        pyautogui.alert('Your lighting conditions are bad. Make sure there is no source of light behind you, '
+                        'but lots of light in front of you.')
